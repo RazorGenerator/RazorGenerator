c9ddc5736ebb4fed5f4504d7ac3f9c231101bd59
davidebbo <none@none>
2010-10-25 14:41:47 -0700

    Initial checkin

---
+++ CompiledWebRazorHost.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.CodeDom;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.WebPages.Razor;
+
+namespace Microsoft.Web.RazorSingleFileGenerator {
+    public class CompiledWebRazorHost : WebPageRazorHost {
+
+
+        public CompiledWebRazorHost(string fileNamespace, string projectRelativePath, string fullPath)
+            : base(GetVirtualPath(projectRelativePath), fullPath) {
+            DefaultNamespace = fileNamespace;
+        }
+
+        protected override string GetClassName(string virtualPath) {
+            virtualPath = virtualPath.TrimStart('~', '/');
+            return Regex.Replace(virtualPath, @"[\/.]", "_");
+        }
+
+        private static string GetVirtualPath(string projectRelativePath) {
+            return VirtualPathUtility.ToAppRelative("~" + projectRelativePath);
+        }
+
+        public override void PostProcessGeneratedCode(CodeCompileUnit codeCompileUnit,
+                                                      CodeNamespace generatedNamespace,
+                                                      CodeTypeDeclaration generatedClass,
+                                                      CodeMemberMethod executeMethod) {
+
+            // Run the base processing
+            base.PostProcessGeneratedCode(codeCompileUnit, generatedNamespace, generatedClass, executeMethod);
+
+            if (!IsSpecialPage) {
+
+
+                // Create the Href wrapper
+                CodeTypeMember hrefMethod = new CodeSnippetTypeMember(String.Format(@"
+                    // Resolve package relative syntax
+                    // Also, if it comes from a static embedded resource, change the path accordingly
+                    public override string Href(string virtualPath, params object[] pathParts) {{
+                        virtualPath = ApplicationPart.ProcessVirtualPath(typeof({0}).Assembly, VirtualPath, virtualPath);
+                        return base.Href(virtualPath, pathParts);
+                    }}
+                    ", generatedClass.Name));
+
+                generatedClass.Members.Add(hrefMethod);
+            }
+
+            // Add a PageVirtualPathAttribute attribute.
+            // TODO: clean this up.  Use Type instead of string once it makes it into Plan9
+            generatedClass.CustomAttributes.Add(
+                new CodeAttributeDeclaration("PageVirtualPathAttribute",
+                    new CodeAttributeArgument(new CodePrimitiveExpression(VirtualPath))));
+        }
+    }
+}
---
+++ RazorClassGenerator.cs
@@ -0,0 +1,129 @@
+/***************************************************************************
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+This code is licensed under the Visual Studio SDK license terms.
+THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+
+***************************************************************************/
+
+using System;
+using System.Runtime.InteropServices;
+using System.CodeDom.Compiler;
+using System.CodeDom;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Schema;
+using Microsoft.Win32;
+using Microsoft.VisualStudio.Shell;
+using VSLangProj80;
+using Microsoft.VisualStudio.Shell.Interop;
+using EnvDTE;
+using System.Web.Razor;
+using System.Web.Razor.Parser;
+using System.Web.Razor.Parser.SyntaxTree;
+
+namespace Microsoft.Web.RazorSingleFileGenerator {
+    /// <summary>
+    /// This is the generator class. 
+    /// When setting the 'Custom Tool' property of a C#, VB, or J# project item to "XmlClassGenerator", 
+    /// the GenerateCode function will get called and will return the contents of the generated file 
+    /// to the project system
+    /// </summary>
+    [ComVisible(true)]
+    [Guid("52B316AA-1997-4c81-9969-83604C09EEB4")]
+    [CodeGeneratorRegistration(typeof(RazorClassGenerator), "C# Razor Generator (.cshtml)", vsContextGuids.vsContextGuidVCSProject, GeneratesDesignTimeSource = true)]
+    //[CodeGeneratorRegistration(typeof(RazorClassGenerator), "VB Razor Generator (.vbhtml)", vsContextGuids.vsContextGuidVBProject, GeneratesDesignTimeSource = true)]
+    [ProvideObject(typeof(RazorClassGenerator))]
+    public class RazorClassGenerator : BaseCodeGeneratorWithSite {
+#pragma warning disable 0414
+        //The name of this generator (use for 'Custom Tool' property of project item)
+        internal static string name = "RazorClassGenerator";
+#pragma warning restore 0414
+
+        /// <summary>
+        /// Function that builds the contents of the generated file based on the contents of the input file
+        /// </summary>
+        /// <param name="inputFileContent">Content of the input file</param>
+        /// <returns>Generated file as a byte array</returns>
+        protected override byte[] GenerateCode(string inputFileContent) {
+            // Get the root folder of the project
+            var appRoot = Path.GetDirectoryName(GetProject().FullName);
+
+            // Determine the project-relative path
+            string projectRelativePath = InputFilePath.Substring(appRoot.Length);
+
+            // Create the host and engine
+            CompiledWebRazorHost host = new CompiledWebRazorHost(FileNameSpace, projectRelativePath, InputFilePath);
+            RazorTemplateEngine engine = new RazorTemplateEngine(host);
+
+            // Generate code
+            GeneratorResults results = null;
+            try {
+                using (TextReader reader = new StringReader(inputFileContent)) {
+                    results = engine.GenerateCode(reader);
+                }
+            }
+            catch (Exception e) {
+                this.GeneratorError(4, e.ToString(), 1, 1);
+                //Returning null signifies that generation has failed
+                return null;
+            }
+
+            // Output errors
+            foreach (RazorError error in results.ParserErrors) {
+                GeneratorError(4, error.Message, (uint)error.Location.LineIndex + 1, (uint)error.Location.CharacterIndex + 1);
+            }
+
+            CodeDomProvider provider = GetCodeProvider();
+
+            try {
+                if (this.CodeGeneratorProgress != null) {
+                    //Report that we are 1/2 done
+                    this.CodeGeneratorProgress.Progress(50, 100);
+                }
+
+                using (StringWriter writer = new StringWriter(new StringBuilder())) {
+                    CodeGeneratorOptions options = new CodeGeneratorOptions();
+                    options.BlankLinesBetweenMembers = false;
+                    options.BracingStyle = "C";
+
+                    //Generate the code
+                    provider.GenerateCodeFromCompileUnit(results.GeneratedCode, writer, options);
+
+                    if (this.CodeGeneratorProgress != null) {
+                        //Report that we are done
+                        this.CodeGeneratorProgress.Progress(100, 100);
+                    }
+                    writer.Flush();
+
+                    //Get the Encoding used by the writer. We're getting the WindowsCodePage encoding, 
+                    //which may not work with all languages
+                    Encoding enc = Encoding.GetEncoding(writer.Encoding.WindowsCodePage);
+
+                    //Get the preamble (byte-order mark) for our encoding
+                    byte[] preamble = enc.GetPreamble();
+                    int preambleLength = preamble.Length;
+
+                    //Convert the writer contents to a byte array
+                    byte[] body = enc.GetBytes(writer.ToString());
+
+                    //Prepend the preamble to body (store result in resized preamble array)
+                    Array.Resize<byte>(ref preamble, preambleLength + body.Length);
+                    Array.Copy(body, 0, preamble, preambleLength, body.Length);
+
+                    //Return the combined byte array
+                    return preamble;
+                }
+            }
+            catch (Exception e) {
+                this.GeneratorError(4, e.ToString(), 1, 1);
+                //Returning null signifies that generation has failed
+                return null;
+            }
+        }
+    }
+}
