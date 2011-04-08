72eb6b1b0990c2a5a30d3d5d4c48c1f0a2dbba7f
pranavkm <none@none>
2011-04-08 10:49:53 -0700

    Adding host based on T4 preprocessed template with example.

---
+++ SampleRazorHelperLibrary/PreProcessedTemplatePartial.cs
@@ -0,0 +1,41 @@
+﻿
+namespace SampleRazorHelperLibrary {
+    /// <remarks>
+    /// Usage Example:
+    /// var test = new Test("MyScope", false, true, true, "Foo", "_foo", "string", "OnChange");
+    /// string result = test.TransformText();
+    /// </remarks>
+    public partial class PreProcessedTemplate {
+        public PreProcessedTemplate(string scope, bool isShared, bool isReadOnly, bool isSetterPrivate,
+                    string propertyName, string backingField, string typeName,
+                    string raisePropertyChangedMethodName) {
+
+            Scope = scope;
+            Shared = isShared;
+            ReadOnly = isReadOnly;
+            IsSetterPrivate = isSetterPrivate;
+            PropertyName = propertyName;
+            BackingField = backingField;
+            TypeName = typeName;
+            RaisePropertyChangedMethodName = raisePropertyChangedMethodName;
+        }
+
+        public string PropertyName { get; set; }
+
+        public string BackingField { get; set; }
+
+        public string TypeName { get; set; }
+
+        public bool Shared { get; set; }
+
+        public string RaisePropertyChangedMethodName { get; set; }
+
+        public bool ReadOnly { get; set; }
+
+        public string Scope { get; set; }
+
+        public bool IsSetterPrivate { get; set; }
+    }
+
+        
+}
