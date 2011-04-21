715b45b0438dea8d28f1ffe3aa278201444dd53d
Jess Chadwick <jess.chadwick@dowjones.com>
2011-04-21 11:38:06 -0400

    Adding Basic Host for very basic templates

---
+++ RazorSingleFileGenerator/RazorHost/BasicHost.cs
@@ -0,0 +1,77 @@
+﻿using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Razor.Parser;
+using System.Web.WebPages.Razor;
+
+namespace Microsoft.Web.RazorSingleFileGenerator.RazorHost
+{
+    public class BasicHost : WebPageRazorHost, ISingleFileGenerator
+    {
+        public static readonly IEnumerable<string> ExcludedNamespaces = new[] {
+                "System.Web.Helpers",
+                "System.Web.WebPages",
+                "System.Web.WebPages.Html",
+            };
+
+
+        public BasicHost(string fileNamespace, string projectRelativePath, string fullPath)
+            : base(GetVirtualPath(projectRelativePath), fullPath)
+        {
+            DefaultDebugCompilation = false;
+            DefaultNamespace = fileNamespace;
+            StaticHelpers = true;
+        }
+
+
+        public virtual void PreCodeGeneration(RazorCodeGenerator codeGenerator, IDictionary<string, string> directives)
+        {
+        }
+
+        public override void PostProcessGeneratedCode(CodeCompileUnit codeCompileUnit, CodeNamespace generatedNamespace, CodeTypeDeclaration generatedClass, CodeMemberMethod executeMethod)
+        {
+            base.PostProcessGeneratedCode(codeCompileUnit, generatedNamespace, generatedClass, executeMethod);
+
+            RemoveExcludedNamespaces(generatedNamespace);
+
+            RemoveApplicationInstanceProperty(generatedClass);
+        }
+
+        protected override string GetClassName(string virtualPath)
+        {
+            string filename = Path.GetFileNameWithoutExtension(virtualPath);
+            return ParserHelpers.SanitizeClassName(filename);
+        }
+
+        protected static string GetVirtualPath(string projectRelativePath)
+        {
+            return VirtualPathUtility.ToAppRelative("~" + projectRelativePath);
+        }
+
+        protected virtual void RemoveApplicationInstanceProperty(CodeTypeDeclaration generatedClass)
+        {
+            CodeMemberProperty applicationInstanceProperty =
+                generatedClass.Members.OfType<CodeMemberProperty>().SingleOrDefault(
+                    p => "ApplicationInstance".Equals(p.Name));
+
+            if (applicationInstanceProperty != null)
+            {
+                generatedClass.Members.Remove(applicationInstanceProperty);
+            }
+        }
+
+        protected virtual void RemoveExcludedNamespaces(CodeNamespace codeNamespace)
+        {
+            List<CodeNamespaceImport> imports =
+                codeNamespace.Imports.OfType<CodeNamespaceImport>()
+                    .Where(import => !ExcludedNamespaces.Contains(import.Namespace))
+                    .ToList();
+
+            codeNamespace.Imports.Clear();
+
+            imports.ForEach(import => codeNamespace.Imports.Add(import));
+        }
+    }
+}
---
+++ SampleRazorHelperLibrary/BasicSampleTemplateBaseClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SampleRazorHelperLibrary
+{
+    /// <summary>
+    /// A very basic template base class which 
+    /// implements the Razor template base class "contract"
+    /// and simply writes to the Console
+    /// </summary>
+    public abstract class BasicSampleTemplateBaseClass
+    {
+        public abstract void Execute();
+
+        protected void Write(object value)
+        {
+            Console.Write(value);
+        }
+
+        protected void WriteLiteral(object value)
+        {
+            Console.Write(value);
+        }
+    }
+}
