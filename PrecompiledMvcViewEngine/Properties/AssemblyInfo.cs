9e04bd2b446ce496d7cdc79c95eb75a64dd2f048
pranavkm <none@none>
2011-06-20 00:07:50 -0700

    Modify PrecompiledMvcViewEngine.csproj to build NuGet package

--- PrecompiledMvcViewEngine/Properties/AssemblyInfo.cs
+++ PrecompiledMvcViewEngine/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PrecompiledMvcView")]
-[assembly: AssemblyDescription("Single File Generator for Razor files (.cshtml files)")]
+[assembly: AssemblyDescription("Mvc View engine that supports precompiled Mvc views")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RazorGenerator contributors")]
 [assembly: AssemblyProduct("PrecompiledMvcView")]
