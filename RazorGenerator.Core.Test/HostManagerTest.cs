81907eab467ab878bb1a6059eab3c5d2af3c0d1e
pranavkm <none@none>
2011-06-22 21:58:04 -0700

    Adding unit test for host detection heuristic
    --HG--
    extra : rebase_source : ec45ad62cceb94c3dc774cdeef063669f978d605

---
+++ RazorGenerator.Core.Test/HostManagerTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RazorGenerator.Core.Test {
+    [TestClass]
+    public class HostManagerTest {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void HostManagerHieruisticForMvcHelperInViewsFolder() {
+            // Arrange
+            var path1 = @"Views\Helper\HtmlExtensions.cshtml";
+            var path2 = @"Views\Shared\Helper\Menu.cshtml";
+            var path3 = @"View\CustomModel.cshtml";
+
+            // Act 
+            var guess1 = HostManager.GuessHost(TestContext.TestDeploymentDir, path1);
+            var guess2 = HostManager.GuessHost(TestContext.TestDeploymentDir, path2);
+            var guess3 = HostManager.GuessHost(TestContext.TestDeploymentDir, path3);
+
+            Assert.AreEqual("MvcHelper", guess1);
+            Assert.AreEqual("MvcHelper", guess2);
+            Assert.IsNull(guess3);
+        }
+
+        [TestMethod]
+        public void HostManagerHieruisticForMvcViews() {
+            // Arrange
+            var path1 = @"Views\Home\About.cshtml";
+            var path2 = @"Views\Shared\_Layout.cshtml";
+
+            // Act 
+            var guess1 = HostManager.GuessHost(TestContext.TestDeploymentDir, path1);
+            var guess2 = HostManager.GuessHost(TestContext.TestDeploymentDir, path2);
+
+            Assert.AreEqual("MvcView", guess1);
+            Assert.AreEqual("MvcView", guess2);
+        }
+    }
+}
