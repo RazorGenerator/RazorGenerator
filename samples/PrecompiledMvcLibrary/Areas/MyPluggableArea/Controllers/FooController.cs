a30ed34c9b39c2d169e05caa2e20b6f2cec74d4a
davidebbo <none@none>
2011-06-29 14:46:29 -0700

    Pluggable area sample

---
+++ samples/PrecompiledMvcLibrary/Areas/MyPluggableArea/Controllers/FooController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrecompiledMvcLibrary.Areas.MyPluggableArea.Controllers {
+    public class FooController : Controller {
+        //
+        // GET: /MyPluggableArea/Foo/
+
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
---
+++ samples/PrecompiledMvcLibrary/Areas/MyPluggableArea/MyAreaAreaRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace PrecompiledMvcLibrary.Areas.MyPluggableArea {
+    public class MyPluggableAreaAreaRegistration : AreaRegistration {
+        public override string AreaName {
+            get {
+                return "MyPluggableArea";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) {
+            context.MapRoute(
+                "MyPluggableArea_default",
+                "MyPluggableArea/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
