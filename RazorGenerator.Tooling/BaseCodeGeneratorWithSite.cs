6799ba4103dc172628a3da1173eb2363e4a2303d
pranavkm <none@none>
2011-07-11 11:02:21 -0700

    Different file name for generated code file Work Item: 8

--- RazorGenerator.Tooling/BaseCodeGeneratorWithSite.cs
+++ RazorGenerator.Tooling/BaseCodeGeneratorWithSite.cs
@@ -22,13 +22,11 @@ using Microsoft.VisualStudio.Shell.Interop;
 using VSLangProj;
 using VSOLE = Microsoft.VisualStudio.OLE.Interop;
 
-namespace RazorGenerator
-{
+namespace RazorGenerator {
     /// <summary>
     /// Base code generator with site implementation
     /// </summary>
-    public abstract class BaseCodeGeneratorWithSite : BaseCodeGenerator, VSOLE.IObjectWithSite
-    {
+    public abstract class BaseCodeGeneratorWithSite : BaseCodeGenerator, VSOLE.IObjectWithSite {
         private object site = null;
         private CodeDomProvider codeDomProvider = null;
         private ServiceProvider serviceProvider = null;
@@ -40,10 +38,8 @@ namespace RazorGenerator
         /// </summary>
         /// <param name="riid">interface to get</param>
         /// <param name="ppvSite">IntPtr in which to stuff return value</param>
-        void VSOLE.IObjectWithSite.GetSite(ref Guid riid, out IntPtr ppvSite)
-        {
-            if (site == null)
-            {
+        void VSOLE.IObjectWithSite.GetSite(ref Guid riid, out IntPtr ppvSite) {
+            if (site == null) {
                 throw new COMException("object is not sited", VSConstants.E_FAIL);
             }
 
@@ -51,8 +47,7 @@ namespace RazorGenerator
             IntPtr intPointer = IntPtr.Zero;
             Marshal.QueryInterface(pUnknownPointer, ref riid, out intPointer);
 
-            if (intPointer == IntPtr.Zero)
-            {
+            if (intPointer == IntPtr.Zero) {
                 throw new COMException("site does not support requested interface", VSConstants.E_NOINTERFACE);
             }
 
@@ -63,8 +58,7 @@ namespace RazorGenerator
         /// SetSite method of IOleObjectWithSite
         /// </summary>
         /// <param name="pUnkSite">site for this object to use</param>
-        void VSOLE.IObjectWithSite.SetSite(object pUnkSite)
-        {
+        void VSOLE.IObjectWithSite.SetSite(object pUnkSite) {
             site = pUnkSite;
             codeDomProvider = null;
             serviceProvider = null;
@@ -75,12 +69,9 @@ namespace RazorGenerator
         /// <summary>
         /// Demand-creates a ServiceProvider
         /// </summary>
-        private ServiceProvider SiteServiceProvider
-        {
-            get
-            {
-                if (serviceProvider == null)
-                {
+        private ServiceProvider SiteServiceProvider {
+            get {
+                if (serviceProvider == null) {
                     serviceProvider = new ServiceProvider(site as VSOLE.IServiceProvider);
                     Debug.Assert(serviceProvider != null, "Unable to get ServiceProvider from site object.");
                 }
@@ -93,8 +84,7 @@ namespace RazorGenerator
         /// </summary>
         /// <param name="serviceGuid">GUID of service to retrieve</param>
         /// <returns>An object that implements the requested service</returns>
-        protected object GetService(Guid serviceGuid)
-        {
+        protected object GetService(Guid serviceGuid) {
             return SiteServiceProvider.GetService(serviceGuid);
         }
 
@@ -103,8 +93,7 @@ namespace RazorGenerator
         /// </summary>
         /// <param name="serviceType">Type of service to retrieve</param>
         /// <returns>An object that implements the requested service</returns>
-        protected object GetService(Type serviceType)
-        {
+        protected object GetService(Type serviceType) {
             return SiteServiceProvider.GetService(serviceType);
         }
 
@@ -113,18 +102,14 @@ namespace RazorGenerator
         /// the project item the generator was called on
         /// </summary>
         /// <returns>A CodeDomProvider object</returns>
-        protected virtual CodeDomProvider GetCodeProvider()
-        {
-            if (codeDomProvider == null)
-            {
+        protected virtual CodeDomProvider GetCodeProvider() {
+            if (codeDomProvider == null) {
                 //Query for IVSMDCodeDomProvider/SVSMDCodeDomProvider for this project type
                 IVSMDCodeDomProvider provider = GetService(typeof(SVSMDCodeDomProvider)) as IVSMDCodeDomProvider;
-                if (provider != null)
-                {
+                if (provider != null) {
                     codeDomProvider = provider.CodeDomProvider as CodeDomProvider;
                 }
-                else
-                {
+                else {
                     //In the case where no language specific CodeDom is available, fall back to C#
                     codeDomProvider = CodeDomProvider.CreateProvider("C#");
                 }
@@ -136,14 +121,12 @@ namespace RazorGenerator
         /// Gets the default extension of the output file from the CodeDomProvider
         /// </summary>
         /// <returns></returns>
-        protected override string GetDefaultExtension()
-        {
+        protected override string GetDefaultExtension() {
             CodeDomProvider codeDom = GetCodeProvider();
             Debug.Assert(codeDom != null, "CodeDomProvider is NULL.");
             string extension = codeDom.FileExtension;
-            if (extension != null && extension.Length > 0)
-            {
-                extension = "." + extension.TrimStart(".".ToCharArray());
+            if (extension != null && extension.Length > 0) {
+                extension = ".Generated." + extension.TrimStart(".".ToCharArray());
             }
             return extension;
         }
@@ -153,8 +136,7 @@ namespace RazorGenerator
         /// generator was called on
         /// </summary>
         /// <returns>The EnvDTE.ProjectItem of the project item the code generator was called on</returns>
-        protected ProjectItem GetProjectItem()
-        {
+        protected ProjectItem GetProjectItem() {
             object p = GetService(typeof(ProjectItem));
             Debug.Assert(p != null, "Unable to get Project Item.");
             return (ProjectItem)p;
@@ -167,8 +149,7 @@ namespace RazorGenerator
         /// <returns>
         /// The EnvDTE.Project object of the project containing the project item the code generator was called on
         /// </returns>
-        protected Project GetProject()
-        {
+        protected Project GetProject() {
             return GetProjectItem().ContainingProject;
         }
 
@@ -177,8 +158,7 @@ namespace RazorGenerator
         /// generator was called on
         /// </summary>
         /// <returns>The VSLangProj.VSProjectItem of the project item the code generator was called on</returns>
-        protected VSProjectItem GetVSProjectItem()
-        {
+        protected VSProjectItem GetVSProjectItem() {
             return (VSProjectItem)GetProjectItem().Object;
         }
 
@@ -190,8 +170,7 @@ namespace RazorGenerator
         /// The VSLangProj.VSProject object of the project containing the project item 
         /// the code generator was called on
         /// </returns>
-        protected VSProject GetVSProject()
-        {
+        protected VSProject GetVSProject() {
             return (VSProject)GetProject().Object;
         }
     }
