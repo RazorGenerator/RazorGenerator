e46b7a2d2a8294fe342872dc7e67d46c3b519adb
pranavkm <none@none>
2011-12-23 08:57:12 -0800

    Work Item: 39 Remove leading underscores from file names.

--- RazorGenerator.Core/CodeTransformers/DirectivesBasedTransformers.cs
+++ RazorGenerator.Core/CodeTransformers/DirectivesBasedTransformers.cs
@@ -7,6 +7,7 @@ namespace RazorGenerator.Core
     {
         public static readonly string TypeVisibilityKey = "TypeVisibility";
         public static readonly string DisableLinePragmasKey = "DisableLinePragmas";
+        public static readonly string TrimLeadingUnderscoresKey = "TrimLeadingUnderscores";
         public static readonly string GenerateAbsolutePathLinePragmas = "GenerateAbsolutePathLinePragmas";
         private readonly List<IRazorCodeTransformer> _transformers = new List<IRazorCodeTransformer>();
 
@@ -22,21 +23,26 @@ namespace RazorGenerator.Core
                 _transformers.Add(new SetTypeVisibility(directives[TypeVisibilityKey]));
             }
 
-            if (IsSwitchEnabled(directives, DisableLinePragmasKey) == true)
+            if (ReadSwitchValue(directives, DisableLinePragmasKey) == true)
             {
                 razorHost.EnableLinePragmas = false;
             }
-            else if (IsSwitchEnabled(directives, GenerateAbsolutePathLinePragmas) != true)
+            else if (ReadSwitchValue(directives, GenerateAbsolutePathLinePragmas) != true)
             {
                 // Rewrite line pragamas to generate bin relative paths instead of absolute paths.
                 _transformers.Add(new RewriteLinePragmas());
             }
 
+            if (ReadSwitchValue(directives, TrimLeadingUnderscoresKey) != false)
+            {
+                // This should in theory be a different transformer.
+                razorHost.DefaultClassName = razorHost.DefaultClassName.TrimStart('_');
+            }
 
             base.Initialize(razorHost, directives);
         }
 
-        private static bool? IsSwitchEnabled(IDictionary<string, string> directives, string key)
+        private static bool? ReadSwitchValue(IDictionary<string, string> directives, string key)
         {
             string value;
             bool switchValue;
