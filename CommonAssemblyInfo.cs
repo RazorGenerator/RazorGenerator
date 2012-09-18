ddca975ada31800158ef86c7283392de79953a2e
davidebbo <david.ebbo@microsoft.com>
2012-09-18 09:58:08 -0700

    Updated HtmlAgilityPack

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.4.1")]
+[assembly: AssemblyVersion("1.4.2")]
 [assembly: AssemblyProduct("RazorGenerator")]
 [assembly: AssemblyCompany("RazorGenerator contributors")]
