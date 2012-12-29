68bcdd3782d85dce3ce8ee8866c0271aa4fb0aa4
davidebbo <david.ebbo@microsoft.com>
2012-12-28 22:25:42 -0800

    Update version to 1.5.2

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.5.1")]
+[assembly: AssemblyVersion("1.5.2")]
 [assembly: AssemblyProduct("RazorGenerator")]
 [assembly: AssemblyCompany("RazorGenerator contributors")]
