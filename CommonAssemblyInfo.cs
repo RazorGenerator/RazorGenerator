a894aa4e205bb6b65657ad728d83033e8b949a94
davidebbo <david.ebbo@microsoft.com>
2013-02-14 14:56:33 -0800

    Up runtime version to 1.5.3

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.5.2")]
+[assembly: AssemblyVersion("1.5.3")]
 [assembly: AssemblyProduct("RazorGenerator")]
 [assembly: AssemblyCompany("RazorGenerator contributors")]
