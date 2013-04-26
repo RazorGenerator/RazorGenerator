68b8ff0e34254ba78236a77b192d3a774469a46f
davidebbo <david.ebbo@microsoft.com>
2013-04-25 22:20:26 -0700

    v2.0: sign all the runtime assemblies

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.5.5")]
+[assembly: AssemblyVersion("2.0.0")]
 [assembly: AssemblyProduct("RazorGenerator")]
 [assembly: AssemblyCompany("RazorGenerator contributors")]
