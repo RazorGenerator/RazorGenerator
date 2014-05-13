322b64e554f35bac52cf5833a4c657e80272e1b7
unknown <jnappi@SPED103.smartpakequine.com>
2014-05-13 16:33:24 -0400

    Added CLSCompliant to CommonAssemblyInfo.cs

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Reflection;
 
 [assembly: AssemblyVersion("2.0.0")]
 [assembly: AssemblyProduct("RazorGenerator")]
 [assembly: AssemblyCompany("RazorGenerator contributors")]
 [assembly: AssemblyInformationalVersion("2.2.2")]
+[assembly: CLSCompliant(true)]
