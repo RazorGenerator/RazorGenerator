dbcd127acb62e1212030ac22ee2b981d4538ecb5
Pranav K <prkrishn@hotmail.com>
2014-07-30 06:33:41 -0700

    Bumping version to 2.2.5

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyVersion("2.0.0")]
 [assembly: AssemblyProduct("RazorGenerator")]
 [assembly: AssemblyCompany("RazorGenerator contributors")]
-[assembly: AssemblyInformationalVersion("2.2.4")]
+[assembly: AssemblyInformationalVersion("2.2.5")]
