d43fe90a72be527526618f28f92ad175f787bc12
Pranav K <prkrishn@hotmail.com>
2014-12-07 19:37:34 -0800

    Updating build scripts to use newer versions of MsBuild if available * Bumping up package version to 2.3.0 * Bumping up vsix version to 1.7.0

--- build.ps1
+++ build.ps1
@@ -1,4 +1,9 @@
-$msbuild = Join-Path $env:Windir "Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
+$potentialMsBuildPaths = @(
+    [IO.Path]::Combine(${env:ProgramFiles(x86)}, "MsBuild", "14.0", "Bin"),
+    [IO.Path]::Combine(${env:ProgramFiles(x86)}, "MsBuild", "12.0", "Bin"),
+    [IO.Path]::Combine($env:Windir, "Microsoft.NET\Framework\v4.0.30319\msbuild.exe")
+)
+$msbuild = Join-Path ($potentialMsBuildPaths | ? { Test-Path $_ } | Select -First 1) "msbuild.exe"
 
 & $msbuild .nuget\nuget.targets /t:_DownloadNuGet /v:M
 .nuget\NuGet.exe restore RazorGenerator.Tooling.sln
