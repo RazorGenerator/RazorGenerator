0aff0e4a992c97cd94f9e7f3cf283f970aac5100
Pranav K <prkrishn@hotmail.com>
2015-02-18 06:47:28 -0800

    Allow building with newer SDKs

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 $potentialMsBuildPaths = @(
-    [IO.Path]::Combine(${env:ProgramFiles(x86)}, "MsBuild", "14.0", "Bin"),
     [IO.Path]::Combine(${env:ProgramFiles(x86)}, "MsBuild", "12.0", "Bin"),
+    [IO.Path]::Combine(${env:ProgramFiles(x86)}, "MsBuild", "14.0", "Bin"),
     [IO.Path]::Combine($env:Windir, "Microsoft.NET\Framework\v4.0.30319\msbuild.exe")
 )
 $msbuild = Join-Path ($potentialMsBuildPaths | ? { Test-Path $_ } | Select -First 1) "msbuild.exe"
