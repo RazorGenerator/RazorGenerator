53e2748bf2020ef132bc017d349106df04f81806
Pranav K <prkrishn@hotmail.com>
2015-08-17 10:18:28 -0700

    Revving assembly info to 2.3.7

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyVersion("2.0.0")]
 [assembly: AssemblyProduct("RazorGenerator")]
 [assembly: AssemblyCompany("RazorGenerator contributors")]
-[assembly: AssemblyInformationalVersion("2.3.6")]
+[assembly: AssemblyInformationalVersion("2.3.7")]
