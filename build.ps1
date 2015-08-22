28af624f47717403e479b5e0c26ce512abf02d73
Atif Aziz <code@raboof.com>
2015-08-22 17:29:55 +0200

    Improvements to MSBuild path building in build.ps1

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
 $potentialMsBuildPaths = @(
     [IO.Path]::Combine(${env:ProgramFiles(x86)}, "MsBuild", "12.0", "Bin"),
     [IO.Path]::Combine(${env:ProgramFiles(x86)}, "MsBuild", "14.0", "Bin"),
-    [IO.Path]::Combine($env:Windir, "Microsoft.NET\Framework\v4.0.30319")
+    [IO.Path]::Combine($env:Windir, "Microsoft.NET", "Framework", "v4.0.30319")
 )
-$msbuild = Join-Path ($potentialMsBuildPaths | ? { Test-Path $_ } | Select -First 1) "msbuild.exe"
+$msbuild = $potentialMsBuildPaths | Join-Path -ChildPath "msbuild.exe" | ? { Test-Path $_ } | Select -First 1
 
 & $msbuild .nuget\nuget.targets /t:_DownloadNuGet /v:M
 .nuget\NuGet.exe restore RazorGenerator.Tooling.sln
