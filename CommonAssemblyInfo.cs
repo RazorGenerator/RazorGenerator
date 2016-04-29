c39ef079e2a994b8c74b29033d2dc4a786c8b606
davidebbo <david.ebbo@microsoft.com>
2016-04-29 12:45:36 -0700

    Runtime 2.4.3

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyVersion("2.0.0")]
 [assembly: AssemblyProduct("RazorGenerator")]
 [assembly: AssemblyCompany("RazorGenerator contributors")]
-[assembly: AssemblyInformationalVersion("2.4.2")]
+[assembly: AssemblyInformationalVersion("2.4.3")]
