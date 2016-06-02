4256508bbac96cc0b351e3af4280c5db4e3500a6
道木 <tiny@sandsea.info>
2016-06-02 11:59:14 +0800

    Use "Func<boo>" instead of bool as the engine property's return type to void issues about null of HttpContext when it used.
    When create the engine from Applciation_Start and used HttpContext to
    set engine's property for some reason we'll get exception about null of
    HttpContext, so use "Func<boo>" instead of bool as the engine property's
    return type will void the issue.

--- RazorGenerator.Mvc/PrecompiledMvcEngine.cs
+++ RazorGenerator.Mvc/PrecompiledMvcEngine.cs
@@ -83,7 +83,7 @@ namespace RazorGenerator.Mvc
         /// <summary>
         /// Determines if IVirtualPathFactory lookups returns files from assembly regardless of whether physical files are available for the virtual path.
         /// </summary>
-        public bool PreemptPhysicalFiles
+        public Func<bool> PreemptPhysicalFiles
         {
             get; set;
         }
@@ -94,7 +94,7 @@ namespace RazorGenerator.Mvc
         /// <remarks>
         /// What an awful name!
         /// </remarks>
-        public bool UsePhysicalViewsIfNewer
+        public Func<bool> UsePhysicalViewsIfNewer
         {
             get; set;
         }
@@ -103,7 +103,7 @@ namespace RazorGenerator.Mvc
         {
             virtualPath = EnsureVirtualPathPrefix(virtualPath);
 
-            if (UsePhysicalViewsIfNewer && IsPhysicalFileNewer(virtualPath))
+            if (UsePhysicalViewsIfNewer() && IsPhysicalFileNewer(virtualPath))
             {
                 // If the physical file on disk is newer and the user's opted in this behavior, serve it instead.
                 return false;
@@ -140,13 +140,13 @@ namespace RazorGenerator.Mvc
             virtualPath = EnsureVirtualPathPrefix(virtualPath);
             Type type;
 
-            if (!PreemptPhysicalFiles && VirtualPathProvider.FileExists(virtualPath))
+            if (!PreemptPhysicalFiles() && VirtualPathProvider.FileExists(virtualPath))
             {
                 // If we aren't pre-empting physical files, use the BuildManager to create _ViewStart instances if the file exists on disk. 
                 return BuildManager.CreateInstanceFromVirtualPath(virtualPath, typeof(WebPageRenderingBase));
             }
 
-            if (UsePhysicalViewsIfNewer && IsPhysicalFileNewer(virtualPath))
+            if (UsePhysicalViewsIfNewer() && IsPhysicalFileNewer(virtualPath))
             {
                 // If the physical file on disk is newer and the user's opted in this behavior, serve it instead.
                 return BuildManager.CreateInstanceFromVirtualPath(virtualPath, typeof(WebViewPage));
