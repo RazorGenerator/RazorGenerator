62b79bcf3ebc9966bbe32dd25c3f15830f48b690
Daxx909 <Daxx909@users.noreply.github.com>
2016-10-12 12:07:06 +0200

    Allow engine to process vbhtml files
    After adding these formats and file extensions, RazorGenerator works for VB.NET.

--- RazorGenerator.Mvc/PrecompiledMvcEngine.cs
+++ RazorGenerator.Mvc/PrecompiledMvcEngine.cs
@@ -40,32 +40,45 @@ namespace RazorGenerator.Mvc
 
             base.AreaViewLocationFormats = new[] {
                 "~/Areas/{2}/Views/{1}/{0}.cshtml", 
+                "~/Areas/{2}/Views/{1}/{0}.vbhtml", 
                 "~/Areas/{2}/Views/Shared/{0}.cshtml", 
+                "~/Areas/{2}/Views/Shared/{0}.vbhtml", 
             };
 
             base.AreaMasterLocationFormats = new[] {
                 "~/Areas/{2}/Views/{1}/{0}.cshtml", 
+                "~/Areas/{2}/Views/{1}/{0}.vbhtml", 
                 "~/Areas/{2}/Views/Shared/{0}.cshtml", 
+                "~/Areas/{2}/Views/Shared/{0}.vbhtml", 
             };
 
             base.AreaPartialViewLocationFormats = new[] {
                 "~/Areas/{2}/Views/{1}/{0}.cshtml", 
+                "~/Areas/{2}/Views/{1}/{0}.vbhtml", 
                 "~/Areas/{2}/Views/Shared/{0}.cshtml", 
+                "~/Areas/{2}/Views/Shared/{0}.vbhtml", 
             };
             base.ViewLocationFormats = new[] {
                 "~/Views/{1}/{0}.cshtml", 
+                "~/Views/{1}/{0}.vbhtml", 
                 "~/Views/Shared/{0}.cshtml", 
+                "~/Views/Shared/{0}.vbhtml", 
             };
             base.MasterLocationFormats = new[] {
                 "~/Views/{1}/{0}.cshtml", 
+                "~/Views/{1}/{0}.vbhtml", 
                 "~/Views/Shared/{0}.cshtml", 
+                "~/Views/Shared/{0}.vbhtml", 
             };
             base.PartialViewLocationFormats = new[] {
                 "~/Views/{1}/{0}.cshtml", 
+                "~/Views/{1}/{0}.vbhtml", 
                 "~/Views/Shared/{0}.cshtml", 
+                "~/Views/Shared/{0}.vbhtml", 
             };
             base.FileExtensions = new[] {
                 "cshtml", 
+                "vbhtml", 
             };
 
             _mappings = (from type in assembly.GetTypes()
