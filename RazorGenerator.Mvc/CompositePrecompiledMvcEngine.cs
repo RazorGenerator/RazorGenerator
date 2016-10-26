bccd5a87c5f4c1fbe824e8e3c2791dfd45e322f6
CZEMacLeod <cynthia.macleod@cloudsoftware.systems>
2016-10-26 14:11:17 +0100

    Adding support for vb.net (.vbhtml) to CompositePrecompiledMvcEngine Fix nuspec against issues #5 and #118 Bump rev to 2.4.9

--- RazorGenerator.Mvc/CompositePrecompiledMvcEngine.cs
+++ RazorGenerator.Mvc/CompositePrecompiledMvcEngine.cs
@@ -23,35 +23,13 @@ namespace RazorGenerator.Mvc
 
         public CompositePrecompiledMvcEngine(IEnumerable<PrecompiledViewAssembly> viewAssemblies, IViewPageActivator viewPageActivator)
         {
-            base.AreaViewLocationFormats = new[] {
-                "~/Areas/{2}/Views/{1}/{0}.cshtml", 
-                "~/Areas/{2}/Views/Shared/{0}.cshtml", 
-            };
-
-            base.AreaMasterLocationFormats = new[] {
-                "~/Areas/{2}/Views/{1}/{0}.cshtml", 
-                "~/Areas/{2}/Views/Shared/{0}.cshtml", 
-            };
-
-            base.AreaPartialViewLocationFormats = new[] {
-                "~/Areas/{2}/Views/{1}/{0}.cshtml", 
-                "~/Areas/{2}/Views/Shared/{0}.cshtml", 
-            };
-            base.ViewLocationFormats = new[] {
-                "~/Views/{1}/{0}.cshtml", 
-                "~/Views/Shared/{0}.cshtml", 
-            };
-            base.MasterLocationFormats = new[] {
-                "~/Views/{1}/{0}.cshtml", 
-                "~/Views/Shared/{0}.cshtml", 
-            };
-            base.PartialViewLocationFormats = new[] {
-                "~/Views/{1}/{0}.cshtml", 
-                "~/Views/Shared/{0}.cshtml", 
-            };
-            base.FileExtensions = new[] {
-                "cshtml", 
-            };
+            base.AreaViewLocationFormats = PrecompiledMvcEngine.DefaultAreaLocationFormats;
+            base.AreaMasterLocationFormats = PrecompiledMvcEngine.DefaultAreaLocationFormats;
+            base.AreaPartialViewLocationFormats = PrecompiledMvcEngine.DefaultAreaLocationFormats;
+            base.ViewLocationFormats = PrecompiledMvcEngine.DefaultLocationFormats;
+            base.MasterLocationFormats = PrecompiledMvcEngine.DefaultLocationFormats;
+            base.PartialViewLocationFormats = PrecompiledMvcEngine.DefaultLocationFormats;
+            base.FileExtensions = PrecompiledMvcEngine.DefaultFileExtensions;
 
             foreach (var viewAssembly in viewAssemblies)
             {
