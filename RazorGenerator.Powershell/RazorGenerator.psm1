3d0b5a3a8d8b8cc4863560aedecc79dbb243420a
Dai Rees <dai@dai.me>
2017-03-01 13:24:15 -0800

    Changing code to get Solution Explorer nodes based on filename from a dictionary/map instead of UI Hierarchy path.

--- RazorGenerator.Powershell/RazorGenerator.psm1
+++ RazorGenerator.Powershell/RazorGenerator.psm1
@@ -14,7 +14,10 @@ function Resolve-ProjectName {
 
 function Get-ProjectFiles {
     Process {
-        if ($_.Kind -eq "{6BB5F8EF-4483-11D3-8BCF-00C04F8EC28C}" -or $_.Kind -eq "{66A26720-8FB5-11D2-AA7E-00C04F688DDE}") {
+		$physicalFolderGuid = "{6BB5F8EF-4483-11D3-8BCF-00C04F8EC28C}" # https://msdn.microsoft.com/en-us/library/bb166496.aspx
+		$solutionFolderGuid = "{66A26720-8FB5-11D2-AA7E-00C04F688DDE}" # https://msdn.microsoft.com/en-us/library/hb23x61k(v=vs.80).aspx
+
+        if ($_.Kind -eq $physicalFolderGuid -or $_.Kind -eq $solutionFolderGuid) {
             $_.ProjectItems | Get-ProjectFiles
         }
         else {
@@ -37,10 +40,43 @@ function Set-CustomTool {
 function Get-RazorFiles {
     param(
         [parameter(ValueFromPipelineByPropertyName = $true)]
-        [string]$ProjectName
+        [string]$projectName
     )
     
-    (Resolve-ProjectName $ProjectName).ProjectItems | Get-ProjectFiles | Where { $_.Name.EndsWith('.cshtml')  }
+    (Resolve-ProjectName $projectName).ProjectItems | Get-ProjectFiles | Where { $_.Name.EndsWith('.cshtml')  }
+}
+
+# Populates a dictionary that maps filenames to Solution Explorer nodes.
+# TODO: Make this function "private", because there's no need for it to be exposed to the user as a PowerShell command.
+function Fill-SolutionExplorerNodes {
+    param(
+        [System.Collections.Generic.Dictionary[System.String,EnvDTE.UIHierarchyItem]]$map,
+        [EnvDTE.UIHierarchyItems]$nodes
+    )
+
+    ForEach( $uiHierarchyItem in $nodes ) {
+        
+        If( !( $uiHierarchyItem -is [EnvDTE.UIHierarchyItem] ) ) {
+            Continue
+        }
+
+        $projectItem = $uiHierarchyItem.Object
+        If( ( $projectItem -is [EnvDTE.ProjectItem] ) -and ( $projectItem.FileNames.Length -eq 1 ) ) {
+            
+            $fileName = $projectItem.FileNames[0]
+            
+            If( !$map.ContainsKey( $fileName ) ) { # guard, because Solution Explorer can have multiple nodes for the same file, for simplicity this code only uses the first appearance.
+                
+                $map.Add( $fileName, $uiHierarchyItem )
+            }
+        }
+
+        If( $uiHierarchyItem -is [EnvDTE.UIHierarchyItems] ) { # only recurse children if the node has children
+            
+            Fill-SolutionExplorerNodes $map $uiHierarchyItem
+        }
+    }
+
 }
 
 function Change-CustomTool {
@@ -50,17 +86,23 @@ function Change-CustomTool {
     )
     
     Process {
-        $solutionExplorer = ($dte.Windows | Where { $_.Type -eq "vsWindowTypeSolutionExplorer" }).Object
+        $solutionExplorer = ($dte.Windows | Where { $_.Type -eq "vsWindowTypeSolutionExplorer" }).Object # returns an object that implements `interface UIHierarchy`: https://msdn.microsoft.com/en-us/library/envdte.uihierarchy.aspx
         $project = (Resolve-ProjectName $projectName)
         $projectPath = [IO.Path]::GetDirectoryName($project.FullName)
         $ProjectName = $project.Name
 		$SolutionName = [IO.Path]::GetFileNameWithoutExtension($dte.Solution.FullName)
         
+        $map = New-Object 'System.Collections.Generic.Dictionary[System.String,EnvDTE.UIHierarchyItem]'
+        Fill-SolutionExplorerNodes $map $solutionExplorer.UIHierarchyItems
+
         Get-RazorFiles $ProjectName | % { 
             if (Set-CustomTool $_ $CustomTool) {
-                $relativePath = Get-RelativePath $projectPath $_
-                if ($relativePath) {
-                    $solutionExplorer.GetItem("$SolutionName\$ProjectName$relativePath").UIHierarchyItems.Expanded = $false
+                $fileName = $_.Properties.Item("FullPath").Value
+                
+                $uiHierarchyItem = $null
+                If( $map.TryGetValue( $fileName, [ref] $uiHierarchyItem ) ) {
+                    
+                    $uiHierarchyItem.UIHierarchyItems.Expanded = $false
                 }
             }
         }
