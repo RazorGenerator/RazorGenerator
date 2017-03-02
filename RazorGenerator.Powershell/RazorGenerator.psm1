6fe1dedf7ca8e4ff3f6e01e9ad7a416a83f409c0
Dai Rees <dai@dai.me>
2017-03-02 11:28:52 -0800

    Consistent capitalization and code formatting. Removing unnecessary Process{} blocks. Removing unused functions.

--- RazorGenerator.Powershell/RazorGenerator.psm1
+++ RazorGenerator.Powershell/RazorGenerator.psm1
@@ -1,36 +1,38 @@
-function Resolve-ProjectName {
-    param(
-        [parameter(ValueFromPipelineByPropertyName = $true)]
-        [string]$ProjectName
+Function Resolve-ProjectName {
+    Param(
+        [parameter(ValueFromPipelineByPropertyName = $true)][string]$projectName
     )
-    
-    if($ProjectName) {
-        Get-Project $ProjectName
+
+    If( $projectName ) {
+        Get-Project $projectName
     }
-    else {
+    Else {
         Get-Project
     }
 }
 
-function Get-ProjectFiles {
-    Process {
-        $physicalFolderGuid = "{6BB5F8EF-4483-11D3-8BCF-00C04F8EC28C}" # https://msdn.microsoft.com/en-us/library/bb166496.aspx
-        $solutionFolderGuid = "{66A26720-8FB5-11D2-AA7E-00C04F688DDE}" # https://msdn.microsoft.com/en-us/library/hb23x61k(v=vs.80).aspx
+Function Get-ProjectFiles {
 
-        if ($_.Kind -eq $physicalFolderGuid -or $_.Kind -eq $solutionFolderGuid) {
-            $_.ProjectItems | Get-ProjectFiles
-        }
-        else {
-            $_
-        }
+    $physicalFolderGuid = "{6BB5F8EF-4483-11D3-8BCF-00C04F8EC28C}" # https://msdn.microsoft.com/en-us/library/bb166496.aspx
+    $solutionFolderGuid = "{66A26720-8FB5-11D2-AA7E-00C04F688DDE}" # https://msdn.microsoft.com/en-us/library/hb23x61k(v=vs.80).aspx
+
+    If( $_.Kind -eq $physicalFolderGuid -or $_.Kind -eq $solutionFolderGuid ) {
+        $_.ProjectItems | Get-ProjectFiles
+    }
+    Else {
+        $_
     }
 }
 
-function Set-CustomTool {
-    param($item, [string]$customTool, [bool]$force = $false)
+Function Set-CustomTool {
+    Param(
+        $item,
+        [string]$customTool,
+        [bool]$force = $false
+    )
     
     $customToolProperty = $_.Properties.Item("CustomTool")
-    if ($force -or ($customTool -ne $customToolProperty.Value)) {
+    If( $force -or ( $customTool -ne $customToolProperty.Value ) ) {
         $customToolProperty.Value = $customTool
         
         Write-Debug "RunCustomTool - $( $_.Name ) - $( $customTool )"
@@ -40,8 +42,8 @@ function Set-CustomTool {
     }
 }
 
-function Get-RazorFiles {
-    param(
+Function Get-RazorFiles {
+    Param(
         [parameter(ValueFromPipelineByPropertyName = $true)]
         [string]$projectName
     )
@@ -49,21 +51,29 @@ function Get-RazorFiles {
     (Resolve-ProjectName $projectName).ProjectItems | Get-ProjectFiles | Where { $_.Name.EndsWith('.cshtml')  }
 }
 
-function Get-SolutionExplorerNodes {
+Function Get-SolutionExplorerNodes {
     
+    $map = New-Object 'System.Collections.Generic.Dictionary[System.String,EnvDTE.UIHierarchyItem]'
+
     # `.Object` returns an object that implements `interface UIHierarchy`: https://msdn.microsoft.com/en-us/library/envdte.uihierarchy.aspx
     $solutionExplorer = ($dte.Windows | Where { $_.Type -eq "vsWindowTypeSolutionExplorer" }).Object 
 
-    $map = New-Object 'System.Collections.Generic.Dictionary[System.String,EnvDTE.UIHierarchyItem]'
-    Fill-SolutionExplorerNodes 1 $map $solutionExplorer.UIHierarchyItems
+    If( $solutionExplorer -ne $null -and $solutionExplorer -is [EnvDTE.UIHierarchy] ) {
+        
+        Fill-SolutionExplorerNodes 1 $map $solutionExplorer.UIHierarchyItems
+    }
+    Else {
+        
+        Write-Debug "Solution Explorer window not found."
+    }
 
     Return $map
 }
 
 # Populates a dictionary that maps filenames to *visible* Solution Explorer nodes. The `.Object` property of each UIHierarchyItem is often, but not necessarily always, an EnvDTE.ProjectItem instance.
 # NOTE: The EnvDTE assemblies are loaded as-per the *.psd1 file's RequiredAssemblies directive.
-function Fill-SolutionExplorerNodes {
-    param(
+Function Fill-SolutionExplorerNodes {
+    Param(
         [int]$depth,
         [System.Collections.Generic.Dictionary[System.String,EnvDTE.UIHierarchyItem]]$map,
         [EnvDTE.UIHierarchyItems]$nodes
@@ -108,96 +118,75 @@ function Fill-SolutionExplorerNodes {
 
 }
 
-function Change-CustomTool {
-    param (
-        [string]$ProjectName,
+Function Change-CustomTool {
+    Param(
+        [string]$projectName,
         [string]$CustomTool
     )
     
-    Process {
-        
-        Set-StrictMode -Version 2.0
+    # StrictMode enables errors about nonexistent properties which is useful when working with COM Automation objects.
+    Set-StrictMode -Version 2.0 
 
-        $project = (Resolve-ProjectName $projectName)
-        $projectPath = [IO.Path]::GetDirectoryName($project.FullName)
-        $projectName = $project.Name
+    $projectName = (Resolve-ProjectName $projectName).Name
         
-        $solutionExplorerNodeMap = Get-SolutionExplorerNodes
+    $solutionExplorerNodeMap = Get-SolutionExplorerNodes
 
-        Get-RazorFiles $projectName | % { # `%` means ForEach-Object
+    Get-RazorFiles $projectName | % { # `%` means ForEach-Object
             
-            # If the RazorGenerator CustomTool is applied to a file not previously marked, then collapse the
-            # node afterwards because VS will show the new children by default, so any *.generated.cs files
-            # that are already visible will not be hidden.
-            if (Set-CustomTool $_ $CustomTool) {
-                $fileName = $_.Properties.Item("FullPath").Value
+        # If the RazorGenerator CustomTool is applied to a file not previously marked, then collapse the
+        # node afterwards because VS will show the new children by default, so any *.generated.cs files
+        # that are already visible will not be hidden.
+        If( Set-CustomTool $_ $CustomTool ) {
+            $fileName = $_.Properties.Item("FullPath").Value
                 
-                $uiHierarchyItem = $null
-                If( $solutionExplorerNodeMap.TryGetValue( $fileName, [ref] $uiHierarchyItem ) ) {
+            $uiHierarchyItem = $null
+            If( $solutionExplorerNodeMap.TryGetValue( $fileName, [ref] $uiHierarchyItem ) ) {
                     
-                    $uiHierarchyItem.UIHierarchyItems.Expanded = $false
-                }
-            }
-        }
-
-        Set-StrictMode -Off
-    }
-}
-
-function Enable-RazorGenerator {
-    param(
-        [parameter(ValueFromPipelineByPropertyName = $true)]
-        [string]$ProjectName
-    )
-
-    Change-CustomTool $ProjectName "RazorGenerator"
-}
-
-function Disable-RazorGenerator {
-    param(
-        [parameter(ValueFromPipelineByPropertyName = $true)]
-        [string]$ProjectName
-    )
-
-    Change-CustomTool $ProjectName ""
-}
-
-function Redo-RazorGenerator {
-    param(
-        [parameter(ValueFromPipelineByPropertyName = $true)]
-        [string]$ProjectName
-    )
-    Process {
-        Get-RazorFiles $ProjectName | % { 
-            if ($_.Properties.Item("CustomTool")) {
-                $_.Object.RunCustomTool()
+                $uiHierarchyItem.UIHierarchyItems.Expanded = $false
             }
         }
     }
+
+    Set-StrictMode -Off
 }
 
-function Get-RelativePath {
-    param($projectPath, $file) 
-    $filePath = $file.Properties.Item("FullPath").Value
-    
-    $index = $filePath.IndexOf($projectPath)
-    if ($index -ge 0) {
-        $filePath.Substring($projectPath.Length)
+Function Enable-RazorGenerator {
+    Param(
+        [parameter(ValueFromPipelineByPropertyName = $true)][string]$projectName
+    )
+
+    Change-CustomTool $projectName "RazorGenerator"
+}
+
+Function Disable-RazorGenerator {
+    Param(
+        [parameter(ValueFromPipelineByPropertyName = $true)][string]$projectName
+    )
+
+    Change-CustomTool $projectName ""
+}
+
+Function Redo-RazorGenerator {
+    Param(
+        [parameter(ValueFromPipelineByPropertyName = $true)][string]$projectName
+    )
+
+    Get-RazorFiles $projectName | % { 
+        If( $_.Properties.Item( "CustomTool" ) ) {
+            $_.Object.RunCustomTool()
+        }
     }
 }
 
 # Expands every `.cshtml` file so its `*.generated.cs` files are visible in Solution Explorer.
-function Show-RazorGeneratedFiles {
-    param(
-        [parameter(ValueFromPipelineByPropertyName = $true)]
-        [string]$projectName
+Function Show-RazorGeneratedFiles {
+    Param(
+        [parameter(ValueFromPipelineByPropertyName = $true)][string]$projectName
     )
 
     Set-StrictMode -Version 2.0
 
-    $project = (Resolve-ProjectName $projectName)
-    $projectPath = [IO.Path]::GetDirectoryName($project.FullName)
-    $projectName = $project.Name
+    $projectName = (Resolve-ProjectName $projectName).Name
         
     $solutionExplorerNodeMap = Get-SolutionExplorerNodes
 
@@ -216,17 +205,14 @@ function Show-RazorGeneratedFiles {
 }
 
 # Collapses every `.cshtml` file so its `*.generated.cs` files are no-longer visible in Solution Explorer.
-function Hide-RazorGeneratedFiles {
-    param(
-        [parameter(ValueFromPipelineByPropertyName = $true)]
-        [string]$projectName
+Function Hide-RazorGeneratedFiles {
+    Param(
+        [parameter(ValueFromPipelineByPropertyName = $true)][string]$projectName
     )
 
     Set-StrictMode -Version 2.0
 
-    $project = (Resolve-ProjectName $projectName)
-    $projectPath = [IO.Path]::GetDirectoryName($project.FullName)
-    $projectName = $project.Name
+    $projectName = (Resolve-ProjectName $projectName).Name
         
     $solutionExplorerNodeMap = Get-SolutionExplorerNodes
 
