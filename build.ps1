43434b818ebf641ad99852172e09ce1a9fb48213
Dai Rees <dai@dai.me>
2021-03-02 05:39:42 -0800

    Updating build.ps1 to use `vswhere`.

--- build.ps1
+++ build.ps1
@@ -1,9 +1,13 @@
-$potentialMsBuildPaths = @(
-    [IO.Path]::Combine(${env:ProgramFiles(x86)}, "MsBuild", "12.0", "Bin"),
-    [IO.Path]::Combine(${env:ProgramFiles(x86)}, "MsBuild", "14.0", "Bin"),
-    [IO.Path]::Combine($env:Windir, "Microsoft.NET", "Framework", "v4.0.30319")
-)
-$msbuild = $potentialMsBuildPaths | Join-Path -ChildPath "msbuild.exe" | ? { Test-Path $_ } | Select -First 1
+#$potentialMsBuildPaths = @(
+#    [IO.Path]::Combine(${env:ProgramFiles(x86)}, "MsBuild", "12.0", "Bin"),
+#    [IO.Path]::Combine(${env:ProgramFiles(x86)}, "MsBuild", "14.0", "Bin"),
+#    [IO.Path]::Combine($env:Windir, "Microsoft.NET", "Framework", "v4.0.30319")
+#)
+#$msbuild = $potentialMsBuildPaths | Join-Path -ChildPath "msbuild.exe" | ? { Test-Path $_ } | Select -First 1
+
+$vswhere = [IO.Path]::Combine(${env:ProgramFiles(x86)}, "Microsoft Visual Studio", "Installer", "vswhere.exe");
+
+$msbuild = & $vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
 
 & $msbuild .nuget\nuget.targets /t:_DownloadNuGet /v:M
 .nuget\NuGet.exe restore RazorGenerator.Tooling.sln
